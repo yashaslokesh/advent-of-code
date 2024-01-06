db509eb9f5d18e8867faca44081149058bb2a98a
Yashas Lokesh <yashas.lokesh@gmail.com>
2024-01-05 20:25:27 -0500

    Update gitignore and add some beginning files

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # advent-of-code
 
+## Rust
 
+- `cargo add [package-name]` to add dependencies
 
 ## Getting started
 
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // let args = std::env::args();
+
+    let day = std::env::args().nth(1);
+    let part = std::env::args().nth(2);
+    let input_file = std::env::args().nth(3);
+
+    println!("Hello, world!");
+}
