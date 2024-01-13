90b5cb26d43056229c6c8f245ae30fa1d03dd2fd
Yashas Lokesh <yashas.lokesh@gmail.com>
2024-01-12 21:13:56 -0500

    Add cabal project for haskell files, move/delete irrelevant files

--- 2023/01a.hs
+++
@@ -1,8 +0,0 @@
--- contents <- readFile "01a.txt"
-
-main = do
-  contents <- readFile "01a.txt"
-  let line <- words contents
-  putStrLn w
-  -- putStrLn "Hello, everybody!"
-  -- putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
